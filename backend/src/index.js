01b674b829448e00c2f0a8f11466be5b58415de3
Arnab Jana <arnab.jana.720@gmail.com>
2025-07-16 10:35:12 +0530

    New CORS Origin Added

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN],
+    origin: [process.env.CORS_ORIGIN, "http://localhost:5173/"],
   })
 );
 
